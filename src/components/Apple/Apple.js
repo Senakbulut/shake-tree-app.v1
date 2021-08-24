ff1479894baedf872c3bbb0a08127b1aca975778
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-08-24 22:29:32 +0300

    component structure added

---
+++ src/components/Apple/Apple.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Apple.css'
+function Apple() {
+    return (
+        <div>
+            <div className="containerApple">
+                    <img src="images/apple.svg" alt="" className="appleImg1" />
+                    <img src="images/apple.svg" alt="" className="appleImg2" />
+                    <img src="images/apple.svg" alt="" className="appleImg3" />
+                    <img src="images/apple.svg" alt="" className="appleImg4" />
+                    <img src="images/apple.svg" alt="" className="appleImg5" />
+                    <img src="images/apple.svg" alt="" className="appleImg6" />
+            </div>
+        </div>
+    )
+}
+
+export default Apple
---
+++ src/components/Tree/Tree.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Tree.css'
+
+function Tree() {
+    return (
+        <div>
+            <div className="containerTree">
+                    <img src="images/tree.svg" alt="" className="treeImg" />
+            </div>
+        </div>
+    )
+}
+
+export default Tree
