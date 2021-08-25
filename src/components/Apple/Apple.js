16524a5ca1f5314d4b47f0f7d0ffcee5c13d1543
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-08-25 14:13:29 +0300

    developed v3

--- src/components/Apple/Apple.js
+++ src/components/Apple/Apple.js
@@ -4,12 +4,21 @@ function Apple() {
     return (
         <div>
             <div className="containerApple">
+
                     <img src="images/apple.svg" alt="" className="appleImg1" />
                     <img src="images/apple.svg" alt="" className="appleImg2" />
+                    <div className="ImgWrapper3">
                     <img src="images/apple.svg" alt="" className="appleImg3" />
+                    </div>
+                    <div className="ImgWrapper4">
                     <img src="images/apple.svg" alt="" className="appleImg4" />
+                    </div>
+                    <div className="ImgWrapper5">
                     <img src="images/apple.svg" alt="" className="appleImg5" />
+                    </div>
+                    <div className="ImgWrapper6">
                     <img src="images/apple.svg" alt="" className="appleImg6" />
+                    </div>
             </div>
         </div>
     )
